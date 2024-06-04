7408da418b8c38344902db9b5bcc9e5a29d53b5a
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-04 17:07:11 +0300

    Question 3, uses filter to get array array of students from a particular location

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,5 @@
 const getStudentsByLocation = (arr, city) => {
-  return arr.filter((student) => {
-    return student.location === city;
-  });
+  return arr.filter((student) => student.location === city );
 };
 
 export default getStudentsByLocation;
