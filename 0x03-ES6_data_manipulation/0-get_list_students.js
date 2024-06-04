be53cb31a232e04c8166dda1414afca5fee285b2
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-04 15:34:30 +0300

    Initial commit

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+export default function getListStudents() {
+    const students = [
+	{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	{ id: 2, firstName: 'James', location: 'Columbia' },
+	{ id: 3, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    return students;
+}
