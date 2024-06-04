ba270fcd2b6dc9fb36d6ac8dc63b9836afb970ae
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-06-04 21:25:38 +0300

    Question 4, data structure manipulation

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,15 @@
 const updateStudentGradeByCity = ((arr, city, grades) => {
   const gradeMaps = new Map(grades.map(item => [item.studentId, item.grade]));
-  const result = arr.filter((student) => student.location === city).map(student => {
-      if (gradeMaps.has(student.id)) {
-	  student.grade = gradeMaps.get(student.id);
+  const cityLocation = arr.filter((student) => student.location === city)
+  const updatedGrades = cityLocation.map(student => {
+    if (gradeMaps.has(student.id)) {
+      student.grade = gradeMaps.get(student.id);
       } else {
-	  student.grade = "N/A";
+	student.grade = "N/A";
       }
       return student;
   });
-    return result;
+  return updatedGrades;
 });
 
 export default updateStudentGradeByCity;
