2cae7f16eb37d9a26266b31e5be9b92095b08c17
mona lisa <williamkubai101@gmail.com>
2024-09-11 20:19:43 +0300

    Js update

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-
 const ClassRoom = class {
-    constructor (maxStudentsSize) {
-	this._maxStudentsSize = maxStudentsSize;
-    }
-}
-
+  constructor (maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+};
 
 module.exports = ClassRoom;
