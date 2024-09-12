5930c226c86a0eecbbb7161fff8cba2e9bced497
mona lisa <williamkubai101@gmail.com>
2024-09-12 11:30:27 +0300

    pricing class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,45 +1,38 @@
-#!/usr/bin/node
+import Currency from './3-currency';
 
-const Currency = require("./3-currency");
+export default class Pricing {
+  constructor(amount, currency) {
+	  if (typeof amount !== 'number' || !(currency instanceof Currency)) throw new Error()
+	  this._amount = amount;
+	  this._currency = currency;
 
-class Pricing {
-    constructor(amount, currency) {
-	if (typeof amount === 'number') {
-	    this._amount = amount;
-	}
+  }
+  get amount() {
+	  return this._amount;
+  }
 
-	if (typeof currency === 'object') {
-	    this._currency = currency;
-	}
-    }
-
-    get amount() {
-	return this._amount;
-    }
-
-    set amount(val) {
-	if (typeof val === 'number') {
+  set amount(val) {
+	  if (typeof val === 'number') {
 	    this._amount = val;
-	}
-    }
+	  }
+  }
 
-    get currency() {
-	return this._currency;
-    }
+  get currency() {
+	  return this._currency;
+  }
 
-    set currency(val) {
-	if (typeof val === 'object') {
+  set currency(val) {
+	  if (typeof val === 'object') {
 	    this._currency = val;
-	}
-    }
+	  }
+  }
 
-    displayFullPrice() {
-	return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-    }
+  displayFullPrice() {
+	  return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-	return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') throw new Error();
+    return amount * conversionRate;
+  }
 }
-
-module.exports = Pricing;
