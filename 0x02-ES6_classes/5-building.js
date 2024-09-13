e1a84d37bc6478e8deccc9a1bcf510abe6c6533a
mona lisa <williamkubai101@gmail.com>
2024-09-13 09:51:31 +0300

    Building class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building{
   constructor(sqft) {
-    if (!typeof sqft === 'number') { throw new Error() }
+    if (typeof sqft !== 'number') throw new Error();
     this._sqft = sqft;
     this.evacuationWarningMessage();
   }
