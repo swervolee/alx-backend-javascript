45534540e1d0052300fc14a47e230cc7fcbd25d5
mona lisa <williamkubai101@gmail.com>
2024-09-13 11:46:18 +0300

    airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,20 +1,27 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') throw new Error();
+    this._name = name;
+    this._code = code;
+  }
 
-class Airport {
-    constructor(name, code) {
-	if (typeof name === 'string') {
-	    this._name = name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	if (typeof code === 'string') {
-	    this._code = code;
-	}
-    }
+  get code() {
+    return this._code;
+  }
 
-    toString() {
-	return (`[object ${this._code}]`);
-    }
+  set name(value) {
+    this._name = value;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
-
-
-module.exports = Airport;
