1627c1bf64068901903fc066c14cb86b5661cee7
mona lisa <williamkubai101@gmail.com>
2024-09-14 14:14:05 +0300

    final code

--- 0x02-ES6_classes/evcar.js
+++ 0x02-ES6_classes/evcar.js
@@ -1,6 +1,6 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
+export default class EVcar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
