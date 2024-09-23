c856402bd296ea48757f776e5f34a8117e5cb565
mona lisa <williamkubai101@gmail.com>
2024-09-23 21:50:46 +0300

    Tty code

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(filename) {
+  try {
+    const fileContents = fs.readFileSync(filename, 'utf8');
+    const lines = fileContents.split('\n').filter(line => line.trim() !== '');
+
+    const fields = {};
+    let totalStudents = 0;
+
+    for (let i = 1; i < lines.length; i++) {
+      const lineContent = lines[i].split(',');
+
+      if (lineContent[3]) {
+        const field = lineContent[3].trim();
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(lineContent[0].trim());
+        totalStudents += 1;
+      }
+    }
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
